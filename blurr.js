3e3a1fa819018e2d457556ca3750166d816f86a6
ayushpanda2005 <2005ayushpanda@gmail.com>
2024-08-30 19:43:12 +0530

    Update blurr.js

--- blurr.js
+++ blurr.js
@@ -18,8 +18,3 @@ const scale = (num , in_min , in_max ,out_min , out_max) => {
     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min ;
 }
 
-<span style="transistion-delay: 0ms">E</span>
-<span style="transistion-delay: 50ms">m</span>
-<span style="transistion-delay: 100ms">a</span>
-<span style="transistion-delay: 150ms">i</span>
-<span style="transistion-delay: 200ms">l</span>
